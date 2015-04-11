bab58fb86eec5311db1f350b432d0f03fdb19bb2
Kevin Nadro <sodapopcuffgames@gmail.com>
2015-04-11 01:03:22 -0500

    First commit
    - Trying to draw bezier curves
    - Fix the selection of control points and end points to move the curve
    around.

---
+++ point.js
@@ -0,0 +1,19 @@
+function point(x,y){
+	this.pos = {x:x, y:y};
+}
+
+
+point.prototype.set = function(x,y){
+	this.pos.x = x;
+	this.pos.y = y;
+}
+
+point.prototype.mp = function(p){
+	var returnPoint = new point(0,0);
+	var x = (this.pos.x + p.pos.x)/2;
+	var y = (this.pos.y + p.pos.y)/2;
+	returnPoint.set(x,y);
+	return returnPoint;
+}
+
+
